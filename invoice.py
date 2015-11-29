f92ba14dced916efedef2b349da0b9b2fc88d75e
resteve <resteve@zikzakmedia.com>
2015-11-29 11:40:16 +0100

    pyflakes

--- invoice.py
+++ invoice.py
@@ -1,9 +1,8 @@
 #This file is part account_invoice_origin_reference module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['InvoiceLine']
 __metaclass__ = PoolMeta
