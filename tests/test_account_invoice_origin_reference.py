be71b59d24372e8ca837e71bb350cb3e7ca2f8cd
resteve <resteve@zikzakmedia.com>
2015-12-01 10:57:40 +0100

    Tests

--- tests/test_account_invoice_origin_reference.py
+++ tests/test_account_invoice_origin_reference.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-#This file is part account_invoice_origin_reference module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part of the account_invoice_origin_reference module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceOriginReferenceTestCase(unittest.TestCase):
+class AccountInvoiceOriginReferenceTestCase(ModuleTestCase):
     'Test Account Invoice Origin Reference module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_origin_reference')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_origin_reference')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_origin_reference'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            AccountInvoiceOriginReferenceTestCase))
+        AccountInvoiceOriginReferenceTestCase))
     return suite
