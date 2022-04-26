2659e8f05cc0a716f86accfacfb001d3dec984b7
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:29 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-#This file is part account_invoice_origin_reference module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-from .test_account_invoice_origin_reference import suite
-
-__all__ = ['suite']
